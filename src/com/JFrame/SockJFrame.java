fa5681345f7c7b2b492856561f022611112054db
Duanwenyu <qddwy15@126.com>
2016-05-23 19:09:29 +0800

    3.0 finish

--- src/com/JFrame/SockJFrame.java
+++ src/com/JFrame/SockJFrame.java
@@ -1,48 +1,56 @@
 package com.JFrame;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.util.Scanner;
+import java.net.UnknownHostException;
 
+import javax.swing.DefaultListModel;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle;
-import javax.swing.WindowConstants;
 import javax.swing.SwingUtilities;
-
+import javax.swing.WindowConstants;
 
 /**
-* This code was edited or generated using CloudGarden's Jigloo
-* SWT/Swing GUI Builder, which is free for non-commercial
-* use. If Jigloo is being used commercially (ie, by a corporation,
-* company or business for any purpose whatever) then you
-* should purchase a license for each developer using Jigloo.
-* Please visit www.cloudgarden.com for details.
-* Use of Jigloo implies acceptance of these licensing terms.
-* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
-* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
-* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
-*/
+ * This code was edited or generated using CloudGarden's Jigloo SWT/Swing GUI
+ * Builder, which is free for non-commercial use. If Jigloo is being used
+ * commercially (ie, by a corporation, company or business for any purpose
+ * whatever) then you should purchase a license for each developer using Jigloo.
+ * Please visit www.cloudgarden.com for details. Use of Jigloo implies
+ * acceptance of these licensing terms. A COMMERCIAL LICENSE HAS NOT BEEN
+ * PURCHASED FOR THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED LEGALLY FOR
+ * ANY CORPORATE OR COMMERCIAL PURPOSE.
+ */
 public class SockJFrame extends javax.swing.JFrame {
 	private JButton send;
 	private JTextField inputTxt;
+	private JButton nameButton;
+	private JLabel sunName;
+	private JList<String> nameList;
+	private JTextField inputName;
+	private JLabel nameLable;
 	private JTextArea showTxt;
-	
+
 	private DataInputStream dis;
 	private DataOutputStream dos;
 	private Socket socket;
 
 	/**
-	* Auto-generated main method to display this JFrame
-	*/
+	 * Auto-generated main method to display this JFrame
+	 */
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
@@ -52,53 +60,77 @@ public class SockJFrame extends javax.swing.JFrame {
 			}
 		});
 	}
-	
+
 	public SockJFrame() {
 		super();
 		initGUI();
 	}
-	
-	private void initGUI() {
-//		String str = null;
-//		Scanner input = new Scanner(System.in);
-		
+
+	public void mainInit() {
 		try {
 			socket = new Socket("127.0.0.1", 8888);
 			System.out.println("客户端已连接！");
 			dos = new DataOutputStream(socket.getOutputStream());
 			dis = new DataInputStream(socket.getInputStream());
-			new Thread() {
-				@Override
-				public void run() {
-					String s = null;
-					try {
-						while (true) {
-							if ((s = dis.readUTF()) != null)
-//								System.out.println(s);
-								showTxt.append(s+"\r\n");
-						}
-					} catch (IOException e) {
-						//e.printStackTrace();
-					}
-				}
-			}.start();
-			
-			GroupLayout thisLayout = new GroupLayout((JComponent)getContentPane());
+			// 不断刷新显示
+						new Thread() {
+							@Override
+							public void run() {
+								String s = null;
+								try {
+									while (true) {
+										if ((s = dis.readUTF()) != null)
+											if(s.startsWith(",")){
+												addList(s);
+											}else{
+											showTxt.append(s + "\r\n");
+											}
+									}
+								} catch (IOException e) {
+									// e.printStackTrace();
+								}
+							}
+						}.start();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void initGUI() {
+		try {
+			GroupLayout thisLayout = new GroupLayout(
+					(JComponent) getContentPane());
 			getContentPane().setLayout(thisLayout);
 			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			// 监听窗口的关闭
+			this.addWindowListener(new WindowAdapter() {
+				public void windowClosing(WindowEvent evt) {
+					// System.out.println("this.windowClosing, event="+evt);
+					try {
+						dis.close();
+						dos.close();
+						socket.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+
 			{
 				send = new JButton();
-				send.setText("Send");
+				send.setText("\u53d1\u9001");
 				send.addMouseListener(new MouseAdapter() {
 					public void mouseClicked(MouseEvent evt) {
-//						sendMouseClicked(evt);
+						// sendMouseClicked(evt);
 						String str = null;
 						str = inputTxt.getText();
 						try {
 							dos.writeUTF(str);
 						} catch (IOException e) {
 							e.printStackTrace();
-						}finally{
+						} finally {
 							inputTxt.setText("");
 						}
 					}
@@ -106,58 +138,246 @@ public class SockJFrame extends javax.swing.JFrame {
 			}
 			{
 				showTxt = new JTextArea();
+				showTxt.setEditable(false);
+			}
+			{
+				nameList = new JList<String>();
+			}
+			{
+				sunName = new JLabel();
+				sunName.setText("\u5728\u7ebf\u7528\u6237\u5217\u8868");
+			}
+			{
+				nameLable = new JLabel();
+				GroupLayout nameLableLayout = new GroupLayout(
+						(JComponent) nameLable);
+				nameLable.setLayout(nameLableLayout);
+				nameLable.setText("Name\uff1a");
+				nameLableLayout.setVerticalGroup(nameLableLayout
+						.createParallelGroup());
+				nameLableLayout.setHorizontalGroup(nameLableLayout
+						.createParallelGroup());
+			}
+			{
+				inputName = new JTextField();
+			}
+			{
+				nameButton = new JButton();
+				nameButton.setText("确认");
+				nameButton.addMouseListener(new MouseAdapter() {
+					public void mouseClicked(MouseEvent evt) {
+
+						String str = null;
+						str = inputName.getText();
+
+						try {
+							dos.writeUTF("#" + str);
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+
+					}
+				});
 			}
 			{
 				inputTxt = new JTextField();
 				inputTxt.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent evt) {
-//						inputTxtActionPerformed(evt);
+						// inputTxtActionPerformed(evt);
 						String str = null;
 						str = inputTxt.getText();
 						try {
 							dos.writeUTF(str);
 						} catch (IOException e) {
 							e.printStackTrace();
-						}finally{
+						} finally {
 							inputTxt.setText("");
 						}
-						
+
 					}
 				});
 			}
-			thisLayout.setVerticalGroup(thisLayout.createSequentialGroup()
-				.addContainerGap()
-				.addComponent(showTxt, GroupLayout.PREFERRED_SIZE, 209, GroupLayout.PREFERRED_SIZE)
-				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-				.addGroup(thisLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-				    .addComponent(send, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
-				    .addComponent(inputTxt, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE))
-				.addContainerGap());
-			thisLayout.setHorizontalGroup(thisLayout.createSequentialGroup()
-				.addContainerGap()
-				.addGroup(thisLayout.createParallelGroup()
-				    .addComponent(showTxt, GroupLayout.Alignment.LEADING, 0, 360, Short.MAX_VALUE)
-				    .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
-				        .addComponent(inputTxt, 0, 292, Short.MAX_VALUE)
-				        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-				        .addComponent(send, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE)))
-				.addContainerGap());
+			thisLayout
+					.setVerticalGroup(thisLayout
+							.createSequentialGroup()
+							.addContainerGap()
+							.addGroup(
+									thisLayout
+											.createParallelGroup()
+											.addGroup(
+													GroupLayout.Alignment.LEADING,
+													thisLayout
+															.createSequentialGroup()
+															.addGroup(
+																	thisLayout
+																			.createParallelGroup(
+																					GroupLayout.Alignment.BASELINE)
+																			.addComponent(
+																					nameLable,
+																					GroupLayout.Alignment.BASELINE,
+																					GroupLayout.PREFERRED_SIZE,
+																					28,
+																					GroupLayout.PREFERRED_SIZE)
+																			.addComponent(
+																					inputName,
+																					GroupLayout.Alignment.BASELINE,
+																					GroupLayout.PREFERRED_SIZE,
+																					GroupLayout.PREFERRED_SIZE,
+																					GroupLayout.PREFERRED_SIZE)
+																			.addComponent(
+																					nameButton,
+																					GroupLayout.Alignment.BASELINE,
+																					GroupLayout.PREFERRED_SIZE,
+																					GroupLayout.PREFERRED_SIZE,
+																					GroupLayout.PREFERRED_SIZE))
+															.addGap(0,
+																	18,
+																	Short.MAX_VALUE)
+															.addComponent(
+																	showTxt,
+																	GroupLayout.PREFERRED_SIZE,
+																	258,
+																	GroupLayout.PREFERRED_SIZE)
+															.addGap(12))
+											.addGroup(
+													GroupLayout.Alignment.LEADING,
+													thisLayout
+															.createSequentialGroup()
+															.addGap(15)
+															.addComponent(
+																	sunName,
+																	GroupLayout.PREFERRED_SIZE,
+																	42,
+																	GroupLayout.PREFERRED_SIZE)
+															.addPreferredGap(
+																	LayoutStyle.ComponentPlacement.RELATED)
+															.addComponent(
+																	nameList,
+																	0,
+																	253,
+																	Short.MAX_VALUE)))
+							.addGroup(
+									thisLayout
+											.createParallelGroup(
+													GroupLayout.Alignment.BASELINE)
+											.addComponent(
+													inputTxt,
+													GroupLayout.Alignment.BASELINE,
+													GroupLayout.PREFERRED_SIZE,
+													30,
+													GroupLayout.PREFERRED_SIZE)
+											.addComponent(
+													send,
+													GroupLayout.Alignment.BASELINE,
+													GroupLayout.PREFERRED_SIZE,
+													29,
+													GroupLayout.PREFERRED_SIZE))
+							.addContainerGap());
+			thisLayout
+					.setHorizontalGroup(thisLayout
+							.createSequentialGroup()
+							.addContainerGap()
+							.addGroup(
+									thisLayout
+											.createParallelGroup()
+											.addGroup(
+													GroupLayout.Alignment.LEADING,
+													thisLayout
+															.createSequentialGroup()
+															.addComponent(
+																	inputTxt,
+																	GroupLayout.PREFERRED_SIZE,
+																	345,
+																	GroupLayout.PREFERRED_SIZE)
+															.addPreferredGap(
+																	LayoutStyle.ComponentPlacement.RELATED)
+															.addComponent(
+																	send,
+																	GroupLayout.PREFERRED_SIZE,
+																	74,
+																	GroupLayout.PREFERRED_SIZE))
+											.addComponent(
+													showTxt,
+													GroupLayout.Alignment.LEADING,
+													GroupLayout.PREFERRED_SIZE,
+													425,
+													GroupLayout.PREFERRED_SIZE)
+											.addGroup(
+													GroupLayout.Alignment.LEADING,
+													thisLayout
+															.createSequentialGroup()
+															.addGap(19)
+															.addComponent(
+																	nameLable,
+																	GroupLayout.PREFERRED_SIZE,
+																	GroupLayout.PREFERRED_SIZE,
+																	GroupLayout.PREFERRED_SIZE)
+															.addPreferredGap(
+																	LayoutStyle.ComponentPlacement.RELATED)
+															.addComponent(
+																	inputName,
+																	GroupLayout.PREFERRED_SIZE,
+																	241,
+																	GroupLayout.PREFERRED_SIZE)
+															.addPreferredGap(
+																	LayoutStyle.ComponentPlacement.UNRELATED)
+															.addComponent(
+																	nameButton,
+																	GroupLayout.PREFERRED_SIZE,
+																	85,
+																	GroupLayout.PREFERRED_SIZE)
+															.addGap(9)))
+							.addGap(23)
+							.addGroup(
+									thisLayout
+											.createParallelGroup()
+											.addGroup(
+													thisLayout
+															.createSequentialGroup()
+															.addComponent(
+																	nameList,
+																	GroupLayout.PREFERRED_SIZE,
+																	148,
+																	GroupLayout.PREFERRED_SIZE)
+															.addGap(0,
+																	0,
+																	Short.MAX_VALUE))
+											.addGroup(
+													GroupLayout.Alignment.LEADING,
+													thisLayout
+															.createSequentialGroup()
+															.addGap(36)
+															.addComponent(
+																	sunName,
+																	GroupLayout.PREFERRED_SIZE,
+																	82,
+																	GroupLayout.PREFERRED_SIZE)
+															.addGap(0,
+																	30,
+																	Short.MAX_VALUE)))
+							.addContainerGap(18, 18));
+
+			// 绘制窗口
 			pack();
-			setSize(400, 300);
+			this.setSize(642, 408);
+
+			// 调用主方法
+			mainInit();
+	
+			
 		} catch (Exception e) {
-		    //add your error handling code here
+			// add your error handling code here
 			e.printStackTrace();
 		}
 	}
-	
-	private void sendMouseClicked(MouseEvent evt) {
-		System.out.println("send.mouseClicked, event="+evt);
-		//TODO add your code for send.mouseClicked
-	}
-	
-	private void inputTxtActionPerformed(ActionEvent evt) {
-		System.out.println("inputTxt.actionPerformed, event="+evt);
-		//TODO add your code for inputTxt.actionPerformed
-	}
 
+	// 方法：在list里面添加数据
+	public void addList(String st) {
+		String[] str=st.split(",");
+		DefaultListModel<String> nal = new DefaultListModel<String>();
+		for (int i = 1; i < str.length; i++) {
+			nal.addElement(str[i]);			
+		}
+		nameList.setModel(nal);
+	}
 }
--- src/com/teacher/TestSockServer.java
+++ src/com/teacher/TestSockServer.java
@@ -9,23 +9,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestSockServer {
+	// 创建用来存多个用户的数组
 	static List<Client> clientList = new ArrayList<Client>();
 
+	// 主方法
 	public void init() {
+		// SeverScoket 和 Socket
 		ServerSocket server = null;
 		Socket socket = null;
+
 		try {
-			// 打开端口，等待服务器连接。
+			// 打开端口，等待服务器连接
 			server = new ServerSocket(8888);
 			System.out.println("服务器已开启！");
-			// 服务端循环监听，若是有客户端连接，服务端接受并且创建一个线程，将连接通道socket放入该线程中，并启动线程。
+
+			// 服务端循环监听，若是有客户端连接，服务端接受并且创建一个线程，将连接通道socket放入该线程中，并启动线程
 			while (true) {
 				socket = server.accept();
-				Client c = new TestSockServer().new Client(socket);
-				clientList.add(c);// 将新连接的客户端存入list中，以便于后面遍历发送信息。
+				Client c = new Client(socket);// ****关于在Main内new内部类的方式 new// Client(socket);
+				clientList.add(c);// 将新连接的客户端存入list中，以便于后面遍历发送信息
 				c.start();
+				updataNameList();
 			}
-
 		} catch (IOException e) {
 			System.out.println("server over!");
 		} finally {
@@ -37,16 +42,48 @@ public class TestSockServer {
 		}
 	}
 
+	// 获得名字的方法
+	public String getName() {
+		String str = ",";
+		for (int i = 0; i < clientList.size(); i++) {
+			if(clientList.get(i).name!=null){
+			str += ("用户:" + clientList.get(i).name);
+			}
+			else{
+				str += ("用户："+clientList.get(i).socket.getPort());
+			}
+			str += ",";
+		}
+		return str;
+	}
+
+	// 更新名字列表
+	public void updataNameList() {
+		String strs=getName();
+		for (int i = 0; i < clientList.size(); i++) {
+			try {
+				if(getName()!=null){
+				new DataOutputStream(clientList.get(i).socket.getOutputStream()).writeUTF(strs);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	// Main方法
 	public static void main(String[] args) {
 		TestSockServer ts = new TestSockServer();
 		ts.init();
 	}
 
-	// 因为需要启用多个线程来完成客户端的连接，所以这里使用内部类。
+	// 因为需要启用多个线程来完成客户端的连接，所以这里使用内部类
+	// 用到多线程，用来创建多个客户端
 	class Client extends Thread {
-		Socket socket = null;
-		DataOutputStream dos = null;
-		DataInputStream dis = null;
+		Socket socket;
+		String name;
+		DataOutputStream dos;
+		DataInputStream dis;
 
 		// 使用带参数的构造方法来将通道放入线程中。
 		public Client(Socket socket) {
@@ -57,24 +94,38 @@ public class TestSockServer {
 		public void run() {
 			String str = null;
 			try {
+				// 在while外创建数据输入输出流，可以省下内存
 				dos = new DataOutputStream(socket.getOutputStream());
 				dis = new DataInputStream(socket.getInputStream());
+
 				// 循环遍历用来接收某个客户端传来的信息，并且将这些信息遍历发送给各个客户端。
 				while (true) {
+					// 判断名字输入格式是否是"#xx"，如果是，就更新昵称
 					if ((str = dis.readUTF()) != null) {
-						// dos.writeUTF(socket.getPort() + "say:" + str);
-						// 循环遍历将信息发送给所有客户端。
-						for (int i = 0; i < clientList.size(); i++) {
-							new DataOutputStream(
-									clientList.get(i).socket.getOutputStream())
-									.writeUTF(socket.getPort() + ":" + str);
+						if (str.startsWith("#")) {
+							this.name = str.substring(1);
+							updataNameList();
+							for (int i = 0; i < clientList.size(); i++) {
+								new DataOutputStream(clientList.get(i).socket.getOutputStream()).writeUTF("用户" + socket.getPort()+ "改名为：" + name);
+							}
+						} else {
+							// 循环遍历将信息发送给所有客户端。
+							for (int i = 0; i < clientList.size(); i++) {
+								if (name != null) {
+									new DataOutputStream(clientList.get(i).socket.getOutputStream()).writeUTF(name + ":" + str);
+								} else {
+									new DataOutputStream(clientList.get(i).socket.getOutputStream()).writeUTF(socket.getPort() + ":"+ str);
+								}
+							}
 						}
 					}
 				}
+
 			} catch (IOException e) {
 				System.out.println("客户端" + socket.getPort() + "退出");
 				clientList.remove(this);
-			    //e.printStackTrace();
+				updataNameList();
+				// e.printStackTrace();
 			} finally {
 				// 关闭各种流
 				try {
